5fdf93d5bb93cd842c8b315ac25686d39ad3d7b2
Jace Browning <jacebrowning@gmail.com>
2016-09-24 19:07:28 -0400

    Add CLI placeholder to test generation

--- osmerge/cli.py
+++ osmerge/cli.py
@@ -6,9 +6,17 @@ import click
 log = logging.getLogger(__name__)
 
 
-@click.command()
+@click.group()
 def main():
     logging.basicConfig(level=logging.INFO)
+    logging.getLogger('yorm').setLevel(logging.WARNING)
+
+
+@main.command()
+def new():
+    # TODO: this logic is only temporary to test the Config model
+    from .models import Config
+    Config.generate_example()
 
 
 if __name__ == '__main__':
